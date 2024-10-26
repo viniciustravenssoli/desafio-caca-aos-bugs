fcd595997efee8c603a8acf5475e9ca0a1af0e6a
vts1978 <vinicioussouza@gmail.com>
2024-10-26 20:37:32 -0300

    fix: initialize Categories list to prevent null reference error

--- bugs/Dima.Web/Pages/Categories/List.razor.cs
+++ bugs/Dima.Web/Pages/Categories/List.razor.cs
@@ -11,7 +11,7 @@ public partial class ListCategoriesPage : ComponentBase
     #region Properties
 
     public bool IsBusy { get; set; } = false;
-    public List<Category> Categories { get; set; }
+    public List<Category> Categories { get; set; } = new List<Category>();
     public string SearchTerm { get; set; } = string.Empty;
 
     #endregion
