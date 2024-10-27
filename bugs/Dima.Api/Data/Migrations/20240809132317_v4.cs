fd34b3862f9d065ac2a35388edcc905919463531
vts1978 <vinicioussouza@gmail.com>
2024-10-27 19:08:48 -0300

    Creating db views

--- bugs/Dima.Api/Data/Migrations/20240809132317_v4.cs
+++ bugs/Dima.Api/Data/Migrations/20240809132317_v4.cs
@@ -335,6 +335,70 @@ namespace Dima.Api.Migrations
                 name: "IX_Transaction_CategoryId",
                 table: "Transaction",
                 column: "CategoryId");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER VIEW [vwGetExpensesByCategory] AS
+            SELECT
+                [Transaction].[UserId],
+                [Category].[Title] AS [Category],
+                YEAR([Transaction].[PaidOrReceivedAt]) AS [Year],
+                SUM([Transaction].[Amount]) AS [Expenses]
+            FROM
+                [Transaction]
+                INNER JOIN [Category] ON [Transaction].[CategoryId] = [Category].[Id]
+            WHERE
+                [Transaction].[PaidOrReceivedAt] >= DATEADD(MONTH, -11, CAST(GETDATE() AS DATE))
+                AND [Transaction].[PaidOrReceivedAt] < DATEADD(MONTH, 1, CAST(GETDATE() AS DATE))
+                AND [Transaction].[Type] = 2
+            GROUP BY
+                [Transaction].[UserId],
+                [Category].[Title],
+                YEAR([Transaction].[PaidOrReceivedAt]);
+            ");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER VIEW [vwGetIncomesAndExpenses] AS
+            SELECT
+                [Transaction].[UserId],
+                MONTH([Transaction].[PaidOrReceivedAt]) AS [Month],
+                YEAR([Transaction].[PaidOrReceivedAt]) AS [Year],
+                SUM(CASE WHEN [Transaction].[Type] = 1 THEN [Transaction].[Amount] ELSE 0 END) AS [Incomes],
+                SUM(CASE WHEN [Transaction].[Type] = 2 THEN [Transaction].[Amount] ELSE 0 END) AS [Expenses]
+            FROM
+                [Transaction]
+            WHERE
+                [Transaction].[PaidOrReceivedAt]
+                    >= DATEADD(MONTH, -11, CAST(GETDATE() AS DATE))
+            AND [Transaction].[PaidOrReceivedAt]
+                < DATEADD(MONTH, 1, CAST(GETDATE() AS DATE))
+            GROUP BY
+                [Transaction].[UserId],
+                MONTH([Transaction].[PaidOrReceivedAt]),
+                YEAR([Transaction].[PaidOrReceivedAt])
+            ");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER VIEW [vwGetIncomesByCategory] AS
+            SELECT
+                [Transaction].[UserId],
+                [Category].[Title] AS [Category],
+                YEAR([Transaction].[PaidOrReceivedAt]) AS [Year],
+                SUM([Transaction].[Amount]) AS [Incomes]
+            FROM
+                [Transaction]
+                    INNER JOIN [Category]
+                            ON [Transaction].[CategoryId] = [Category].[Id]
+            WHERE
+                [Transaction].[PaidOrReceivedAt]
+                    >= DATEADD(MONTH, -11, CAST(GETDATE() AS DATE))
+            AND [Transaction].[PaidOrReceivedAt]
+                < DATEADD(MONTH, 1, CAST(GETDATE() AS DATE))
+            AND [Transaction].[Type] = 1
+            GROUP BY
+                [Transaction].[UserId],
+                [Category].[Title],
+                YEAR([Transaction].[PaidOrReceivedAt])
+            ");
         }
 
         /// <inheritdoc />
