1f4589f1d6f5829ab7a3819e8c149e7a68e5ae61
vts1978 <vinicioussouza@gmail.com>
2024-10-27 14:38:44 -0300

    Implement UpdateAsync method for updating transactions

--- bugs/Dima.Api/Handlers/TransactionHandler.cs
+++ bugs/Dima.Api/Handlers/TransactionHandler.cs
@@ -43,7 +43,30 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
 
     public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var transaction = await context
+                .Transactions
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (transaction is null)
+                return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+            transaction.Title = request.Title;
+            transaction.CategoryId = request.CategoryId;
+            transaction.Type = request.Type;
+            transaction.Amount = request.Amount;
+            transaction.PaidOrReceivedAt = request.PaidOrReceivedAt;
+
+            context.Transactions.Update(transaction);
+            await context.SaveChangesAsync();
+
+            return new Response<Transaction?>(transaction, message: "Transação atualizada com sucesso");
+        }
+        catch
+        {
+            return new Response<Transaction?>(null, 500, "Não foi possível alterar a transação");
+        }
     }
 
     public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
