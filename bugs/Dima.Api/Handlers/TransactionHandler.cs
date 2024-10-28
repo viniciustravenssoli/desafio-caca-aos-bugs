edd0ca5c30b51bc24575588095dd53d438e2ba2c
vts1978 <vinicioussouza@gmail.com>
2024-10-28 19:02:29 -0300

    fix: now work for all users not only the test one

--- bugs/Dima.Api/Handlers/TransactionHandler.cs
+++ bugs/Dima.Api/Handlers/TransactionHandler.cs
@@ -21,7 +21,7 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
         {
             var transaction = new Transaction
             {
-                UserId = "teste@balta.io",
+                UserId = request.UserId,
                 CategoryId = request.CategoryId,
                 CreatedAt = DateTime.Now,
                 Amount = request.Amount,
